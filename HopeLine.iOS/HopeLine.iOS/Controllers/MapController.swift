e779509bdab79cad09e9824c9ca01dede2ef8ceb
ejricahuerta <exricahuerta@gmail.com>
2018-11-01 15:48:43 -0400

    initial mainboard and controllers

---
+++ HopeLine.iOS/HopeLine.iOS/Controllers/MapController.swift
@@ -0,0 +1,35 @@
+//
+//  MapController.swift
+//  HopeLine.iOS
+//
+//  Created by Edmel John Ricahuerta on 2018-11-01.
+//  Copyright © 2018 prj. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

125fdcefc4f717626617bbd14733a816c7b82f6d
ejricahuerta <exricahuerta@gmail.com>
2018-11-02 18:40:58 -0400

    laout for auth view change

---
+++ HopeLine.iOS/HopeLine.iOS/ViewModels/PrimaryButton.swift
@@ -0,0 +1,21 @@
+//
+//  PrimaryButton.swift
+//  HopeLine.iOS
+//
+//  Created by Edmel John Ricahuerta on 2018-11-02.
+//  Copyright © 2018 prj. All rights reserved.
+//
+
+import UIKit
+
+class PrimaryButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HopeLine.iOS/HopeLine.iOS/ViewModels/Topics.swift
@@ -0,0 +1,13 @@
+//
+//  Topics.swift
+//  HopeLine.iOS
+//
+//  Created by Edmel John Ricahuerta on 2018-11-02.
+//  Copyright © 2018 prj. All rights reserved.
+//
+
+import UIKit
+
+class Topics: NSObject {
+
+}
